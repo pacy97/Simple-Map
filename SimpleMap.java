ab80e2170bfa3494083b9c2642c4e6fd2e18fd96
Pacy <40542052+pacy97@users.noreply.github.com>
2018-07-08 13:36:44 +0530

    Update SimpleMap.java

--- SimpleMap.java
+++ SimpleMap.java
@@ -9,20 +9,29 @@ import android.support.v7.app.AppCompatActivity;
  * Created by sumith on 16-10-2017.
  */
 
-public class about extends AppCompatActivity {
+public class about extends FragmentActivity implements OnMapReadyCallback  {
 
-private Button button;
+private GoogleMap mMap;
+
 
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
-        button=(TextView)findViewById(R.id.btn);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               //Add your operation here.......
-            }
-        });
+                // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
